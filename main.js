a6ed0dfbebe9737e41506a77a16dfc712d903c95
Vincent Jo <chiwoojo@gmail.com>
2015-10-14 15:02:54 -0700

    initial commit - End of 1-1
    This is me following along with Angular JS tutorial called “Shaping Up
    with Angular” in code school and taking notes on it.

---
+++ main.js
@@ -0,0 +1,35 @@
+
+
+
+    
+    (function() {
+        var app = angular.module('store', []); 
+        //modules are like encapsulations of code
+
+        app.controller('StoreController', function() {
+        //controllers define behaviors and values 
+            this.products = gems;
+            //"product is now a property of the controller"
+        });
+
+        var gems = [ 
+        {
+            name: 'Dodecahedron',
+            price: 2.95,
+            description: '. . .',
+            canPurchase: true,
+            soldOut: false
+        }, 
+        {
+            name: "Pentagonal Gem",
+            price: 5.95,
+            description: " . . . ",
+            canPurchase: true
+        }
+        ];
+    })();
+    //The reason why there is a () at the end here is for this function to play when the file loads. If you didn't have it load, there would be no 'app' to use with the Angular Directive
+        
+
+    
+
